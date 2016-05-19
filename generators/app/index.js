ffc92a2a5b4bf69724312b31a83974fee216283b
James Florentino <jamesflorentino@gmail.com>
2016-05-19 14:45:49 +0800

    Fix copy/directory parameters

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,11 +3,11 @@ var generators = require('yeoman-generator');
 module.exports = generators.Base.extend({
   index() {
     this.copy('gitignore', '.gitignore')
-    this.copy('.jshintrc')
     this.copy('gulpfile.js')
     this.copy('package.json')
     this.copy('yuidoc.json')
-    this.fs.copy(this.templatePath('public/**/*'), this.destinationRoot('public'))
-    this.fs.copy(this.templatePath('src/**/*'), this.destinationRoot('src'))
+    this.directory('src')
+    this.directory('public')
+    this.bulkDirectory('yuidoc-themes', 'yuidoc-themes')
   }
 });
